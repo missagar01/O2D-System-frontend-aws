e8392340a8eeda88705cf13f6fb00080e3f1e6f1
Sagar <mis2@sagartmt.com>
2025-12-10 18:15:54 +0530

    add this  update code

--- components/payment-view.tsx
+++ components/payment-view.tsx
@@ -429,6 +429,7 @@ export function PaymentView() {
                   placeholder="Search customer..."
                   className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 pr-10"
                 />
+                
                 {customerSearch && (
                   <button
                     type="button"
